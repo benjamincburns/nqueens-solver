c2115609514913c6c7fb12ada6b053bb6a8d8b62
Ben Burns <benjamin.c.burns@gmail.com>
2016-05-16 21:55:57 +1200

    better commenting in propositional_nqueens

--- propositional_nqueens.py
+++ propositional_nqueens.py
@@ -8,6 +8,55 @@ from checker import Checker
 import sys
 
 
+def propositional_nqueens(n):
+    """Solves the problem of the nqueens thanks to constraint programming/z3 & a little trick"""
+
+    queens = Function('queens', IntSort(), IntSort(), BoolSort())
+    x = Int('x')
+    y = Int('y')
+
+    expressions = []
+    s = Solver()
+
+    expressions.append(And(x >= 0, x < n))
+    expressions.append(And(y >= 0, y < n))
+
+    for val in xrange(n):
+        # some column must be True for row `val`
+        expressions.append(queens(val, y) == True)
+
+        # some row must be True for column `val`
+        expressions.append(queens(x, val) == True)
+
+    for point1 in points(n):
+        for point2 in points(n, point1[0] + 1, point1[1] + 1):
+
+            # no two queens may occupy the same row, column, or diagonal
+            if are_same_row(point1, point2) or are_same_col(point1, point2) or are_diagonal(point1, point2):
+                expressions.append(Implies(queens(point1[0], point1[1]), Not(queens(point2[0], point2[1]))))
+
+            for point3 in points(n, point2[1] + 1, point2[1] + 1):
+
+                # no three queens may be colinear (fall on the same line of arbitrary angle)
+                if are_colinear(point1, point2, point3):
+                    expressions.append(Implies(And(queens(point1[0], point1[1]), queens(point2[0], point2[1])), Not(queens(point3[0], point3[1]))))
+
+
+    # ugly, but makes it easier to dump expressions to SMT
+    for expression in expressions:
+        s.add(expression)
+
+    if s.check() == unsat:
+        # dat error checking
+        raise Exception('Unsat.')
+
+    m = s.model()
+
+    # incomprehensible list comprehensions FTW
+    results = sorted([(a[0].as_long(),a[1].as_long()) for a in m[queens].as_list() if hasattr(a, '__getitem__') and is_true(a[2])])
+
+    return results
+
 def points(n, x_start = 0, y_start = 0):
     for x in xrange(x_start, n):
         for y in xrange(y_start, n):
@@ -30,44 +79,6 @@ def are_colinear(point1, point2, point3):
 
     return rise1 * run2 == rise2 * run1
 
-def propositional_nqueens(n):
-    """Solves the problem of the nqueens thanks to constraint programming/z3 & a little trick"""
-
-    queens = Function('queens', IntSort(), IntSort(), BoolSort())
-    x = Int('x')
-    y = Int('y')
-
-    expressions = []
-    s = Solver()
-
-    expressions.append(And(x >= 0, x < n))
-    expressions.append(And(y >= 0, y < n))
-
-    for val in xrange(n):
-        expressions.append(queens(val, y) == True)
-        expressions.append(queens(x, val) == True)
-
-    for point1 in points(n):
-        for point2 in points(n, point1[0] + 1, point1[1] + 1):
-            if are_same_row(point1, point2) or are_same_col(point1, point2) or are_diagonal(point1, point2):
-                expressions.append(Implies(queens(point1[0], point1[1]), Not(queens(point2[0], point2[1]))))
-
-            for point3 in points(n, point2[1] + 1, point2[1] + 1):
-                if are_colinear(point1, point2, point3):
-                    expressions.append(Implies(And(queens(point1[0], point1[1]), queens(point2[0], point2[1])), Not(queens(point3[0], point3[1]))))
-
-
-    for expression in expressions:
-        s.add(expression)
-
-    if s.check() == unsat:
-       raise Exception('Unsat.')
-
-    m = s.model()
-    #return m
-
-    results = sorted([(a[0].as_long(),a[1].as_long()) for a in m[queens].as_list() if hasattr(a, '__getitem__') and is_true(a[2])])
-    return results
 
 def display_solutions(s):
     print '# N = %s' % len(s)
